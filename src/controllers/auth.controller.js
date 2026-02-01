b667e5d4b3a52fc99dca93935bb530e4358d8b2b
lukasz2002 <lukasz.kleina02@gmail.com>
2026-02-01 23:01:16 +0100

    changed cookie sameSite to none

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -79,7 +79,7 @@ const login = async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
@@ -90,7 +90,7 @@ const logout = async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
   });
 
   res.json({ message: "Logged out successfully" });
